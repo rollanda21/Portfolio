9638a62cbb3ef74855817efb28bbb5a1618dbcfb
Rolla Nda <rollanda21@gmail.com>
2022-01-03 06:28:25 +0100

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import './App.css';
 
 function App() {
   return (
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,7 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div> */
+    <h2> ... </h2>
   );
 }
 
