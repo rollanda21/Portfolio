82fbd6c8e87582f1c37ff6724ffef03698e3498e
Rolla Nda <rollanda21@gmail.com>
2022-01-19 01:12:26 +0100

    Header completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 /*import logo from './logo.svg';*/
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+/*import Particles from "react-particles-js"*/
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Header />
+
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react"
+import Typed from "react-typed"
+
+
+const Header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="main-info">
+                <h1>The future is now</h1>
+                <Typed 
+                    className="typed-text"
+                    strings={["Machine learning", "Deep learning", "Python", "Software engineering"]}
+                    typeSpeed={40}
+                    backSpeed={60}
+                    loop
+                />
+                <a href="#" className="btn-main-offer"></a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
