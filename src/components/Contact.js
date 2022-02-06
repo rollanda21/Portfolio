9745540f4a18591f772c509b6122b3bbaf0b6b69
Rolla Nda <rollanda21@gmail.com>
2022-02-07 00:11:21 +0100

    Contact design completed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,15 +5,14 @@ const shake = require('../images/shake.png');
 function Contact () {
     return (
         <div className="contact" id="contact">
-            <div className="left">
-                <img src={shake} alt=""/>
-            </div>
-            <div className="right">
-                <h2>Contact</h2>
+            
+            <div className="container">
+                <h1>Contact</h1>
+                <h4>Hit me up width this form below, I will reply asap.</h4>
                 <form>
                     <input type="text" placeholder="Email"/>
                     <textarea placeholder="Message" ></textarea>
-                    <button type="submit">Send</button>
+                    <button type="submit" className="btn btn-dark">SEND</button>
                 </form>
             </div>
             
