4213023b8acde2f857148d9d0231e14d0ce19356
Rolla Nda <rollanda21@gmail.com>
2022-02-06 02:55:04 +0100

    Designing contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,21 @@
 import React from "react"
+const shake = require('../images/shake.png');
 
 
 function Contact () {
     return (
         <div className="contact" id="contact">
-            contact
+            <div className="left">
+                <img src={shake} alt=""/>
+            </div>
+            <div className="right">
+                <h2>Contact</h2>
+                <form>
+                    <input type="text" placeholder="Email"/>
+                    <textarea placeholder="Message" ></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
             
         </div>
     )
