3fdc511f26a1ece01aa04c4e804ec49ad80d4d4b
Rolla Nda <rollanda21@gmail.com>
2022-02-06 01:21:21 +0100

    Testimonials component completed!

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -7,24 +7,62 @@ const arrow_right = require('../images/arrow_right.png');
 const girl = require('../images/girl.jpg');
 
 function Testimonials() {
+
+    const data = [
+        {
+            id: 1,
+            name: "Bob",
+            title: "CEO RollaTrans",
+            img: {girl},
+            icon: {youtube_icon},
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+            featured: true,
+
+        },
+        {
+            id: 1,
+            name: "Clara",
+            title: "CEO RollaTrans",
+            img: {girl},
+            icon: {youtube_icon},
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+            featured: true,
+
+        },
+        {
+            id: 1,
+            name: "Paula",
+            title: "CEO RollaTrans",
+            img: {girl},
+            icon: {youtube_icon},
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+            featured: true,
+
+        },
+        
+    ];
+
     return (
         <div className="testimonials" id="testimonials">
             <h1>testimonials</h1>
             <div className="container">
-                <div className="card">
+                {data.map(d => (
+
+                    <div className="card">
                     <div className="top">
                         <img src={arrow_right} alt="" className="left"/>
                         <img src={girl} alt="" className="user"/>
                         <img src={youtube_icon} alt="" className="right"/>
                     </div>
                     <div className="center">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p>{d.desc} </p>
                     </div>
                     <div className="bottom">
-                        <h3>Name</h3>
-                        <h4>Job title </h4>
+                        <h3>{d.name} </h3>
+                        <h4>{d.title} </h4>
                     </div>
-                </div>
+                    </div>
+                ))}
             </div>
             
                 
