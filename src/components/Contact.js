33e42a39dd9cef7523f2bad3aef7bea41d687bf9
Rolla Nda <rollanda21@gmail.com>
2022-02-07 14:29:33 +0100

    Set up the emailjs API for the contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,23 @@
-import React from "react"
-const shake = require('../images/shake.png');
+import React, {useRef} from "react";
+import emailjs from '@emailjs/browser';
+
+
 
 
 function Contact () {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_5zobpti', 'template_f7v4soc', form.current, 'user_DSTbPrghazJ7RRma8cRQn')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+
     return (
         <div className="contact" id="contact">
             
@@ -20,27 +35,19 @@ function Contact () {
                     </div>
                 </div>
                 <div className="row">
-                     <form>
+                     <form ref={form} onSubmit={sendEmail}>
                          
-                      <div className="mb-3"><input type="text" className="form-control" placeholder="Email"/></div>
+                      <div className="mb-3"><input type="text" className="form-control" placeholder="Email" /></div>
                      <div className="mb-3"><textarea className="form-control" placeholder="Message" ></textarea></div>
                         
                         <button type="submit" className="btn btn-dark">SEND</button>
                      </form>
-                </div>
-
-                    
-
-                        
-                    
-                   
-
-                
+                </div> 
                 
             </div>
             
         </div>
     )
-}
+    }
 
 export default Contact
