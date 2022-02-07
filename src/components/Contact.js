ef6618042535f88bc736468eb8d83012724abd27
Rolla Nda <rollanda21@gmail.com>
2022-02-07 13:26:07 +0100

    Displayed contact component with bootstrap grid

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,14 +6,37 @@ function Contact () {
     return (
         <div className="contact" id="contact">
             
-            <div className="container">
-                <h1>Contact</h1>
-                <h4>Hit me up width this form below, I will reply asap.</h4>
-                <form>
-                    <input type="text" placeholder="Email"/>
-                    <textarea placeholder="Message" ></textarea>
-                    <button type="submit" className="btn btn-dark">SEND</button>
-                </form>
+            <div className="container-fluid">
+
+                <div className="row">
+                    <div className="col-sm-12">
+
+                        <h1 className="text-center">Contact</h1>
+                    </div>
+
+                    <div className="col-sm-12">
+
+                        <h4 className="text-center">Hit me up width this form below, I will reply asap.</h4>
+                    </div>
+                </div>
+                <div className="row">
+                     <form>
+                         
+                      <div className="mb-3"><input type="text" className="form-control" placeholder="Email"/></div>
+                     <div className="mb-3"><textarea className="form-control" placeholder="Message" ></textarea></div>
+                        
+                        <button type="submit" className="btn btn-dark">SEND</button>
+                     </form>
+                </div>
+
+                    
+
+                        
+                    
+                   
+
+                
+                
             </div>
             
         </div>
